e491ea217c076480edc2740a11bfff0ee55bf7c0
test <test>
2023-03-22 16:04:08 -0600

    Updates

--- docker/cachelib.sh
+++ docker/cachelib.sh
@@ -4,21 +4,13 @@ git clone -b develop https://github.com/intel/CacheLib CacheLib
 
 cd CacheLib
 
-git checkout 265c312de090cc735fb51d278a116c360bab19c8
-
-# Add multi-tier navy benchmark (21 Dec 2022)
-./contrib/prerequisites-centos8.sh
-
-for pkg in zstd googleflags googlelog googletest sparsemap fmt folly fizz wangle fbthrift ;
-do
-    ./contrib/build-package.sh -j -I /opt/ "$pkg"
-done
+git checkout f4e30a7f3c878bc119577a11d2ca69c72d41a925
 
 mkdir build
 cd build
 
-cmake ../cachelib -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt
-make install -j $(nproc)
+cmake ../cachelib -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/opt -DCMAKE_BUILD_TESTS=0
+make install
 
 cd ../..
 rm -rf CacheLib
